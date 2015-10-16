adb7d99f88eab2bc26b49cf9d4623d66e5c08a62
Menkou Andrei <nothingelsematters7@gmail.com>
2015-10-16 18:50:42 +0300

    Add vhost blank symbol

--- producer/producer.go
+++ producer/producer.go
@@ -15,7 +15,7 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/%2F")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
