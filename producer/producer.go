8e53279f82f95d513d8355a761d23ded824225ab
Menkov Andrey <a_menkov@wargaming.net>
2015-10-19 12:15:50 +0300

    Produce message in json format

--- producer/producer.go
+++ producer/producer.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 
 	"github.com/nothingelsematters7/golang_rabbit/config"
 	"github.com/streadway/amqp"
 )
+
+// Message provides container for message to be sent and received
+type Message struct {
+	Type string
+	Data map[string]string
+}
 
 func failOnError(err error, msg string) {
 	if err != nil {
@@ -35,15 +42,22 @@ func main() {
 	)
 	failOnError(err, "Failed to declare a queue")
 
-	body := "hello"
+	data := map[string]string{
+		"order_id": "1",
+		"amount":   "10.00"}
+
+	message := Message{"Update", data}
+	body, err := json.Marshal(message)
+	failOnError(err, "Failed to dump message to json")
+
 	err = ch.Publish(
 		"",     // exchange
 		q.Name, // routing key
 		false,  // mandatory
 		false,  // immediate
 		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
+			ContentType: "application/json",
+			Body:        body,
 		})
 	log.Printf(" [x] Sent %s", body)
 	failOnError(err, "Failed to publish a message")
